d8b026206d22292a612e582f06542ff0ecb5f63d
steven2801 <ssteven075@gmail.com>
2023-12-19 20:10:30 +0700

    setup linter, init tailwind config

--- web/pages/_app.tsx
+++ web/pages/_app.tsx
@@ -1,6 +1,22 @@
-import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Lexend } from 'next/font/google'
+import { ThemeProvider } from 'next-themes'
+
+import '@/styles/globals.css'
+
+const lexend = Lexend({ subsets: ['latin'] })
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <style jsx global>{`
+        :root {
+          --font-sans: ${lexend.style.fontFamily};
+        }
+      `}</style>
+      <ThemeProvider forcedTheme="dark">
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  )
 }
---
+++ web/tailwind.config.ts
@@ -0,0 +1,22 @@
+import type { Config } from 'tailwindcss'
+import { fontFamily } from 'tailwindcss/defaultTheme'
+
+const config: Config = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+    './modules/**/*.{js,ts,jsx,tsx}',
+  ],
+  future: {
+    hoverOnlyWhenSupported: true,
+  },
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['var(--font-sans)', ...fontFamily.sans],
+      },
+    },
+  },
+  plugins: [],
+}
+export default config
